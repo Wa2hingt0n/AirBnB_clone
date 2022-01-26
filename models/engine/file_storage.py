2641c33928f0054fa136c6824f24513f5bf20aa1
Washington Obungu <tobungu@gmail.com>
2022-01-26 07:49:16 -0800

    Created the class FileStorage

---
+++ models/engine/file_storage.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+""" Defines a class FileStorage for storing instances to a JSON file """
+
+
+import json
+import os.path
+
+
+class FileStorage:
+    """ Serializes instances to a JSON file and deserializes JSON file to
+    instances """
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """ Returns a dictionary of all objects """
+        return FileStorage.__objects
+
+    def new(self, obj):
+        """ Sets the object to be stored in the __object dictionary by its
+        classname and id
+
+        Args:
+            obj: The object to be set
+        """
+        FileStorage.__objects[obj.__class__.__name__ + "." +
+                              obj.id] = obj
+
+    def save(self):
+        """ Serializes the dictionary of objects to a JSON file """
+        obj_dict = {}
+        for k, v in FileStorage.__objects.items():
+            obj_dict[k] = v.to_dict()
+
+        with open(FileStorage.__file_path, 'w') as f:
+            json_string = json.dumps(obj_dict)
+            f.write(json_string)
+
+    def reload(self):
+        """ Deserializes a json string back to an instance """
+        if os.path.isfile(FileStorage.__file_path):
+            with open(FileStorage.__file_path, 'r') as f:
+                FileStorage.__objects = json.load(f)
